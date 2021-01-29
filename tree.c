adbcf3f087b79f7051b0cc5e95079447471c2f0e
Jayasankar JP <scicopath01@gmail.com>
2021-01-29 14:48:28 +0000

    add inorder preorder and postorder

--- tree.c
+++ tree.c
@@ -11,20 +11,48 @@ struct node *create(){
     newNode->left=NULL;
     newNode->right=NULL;
     printf("Are you enetering any data (Y/N) ? : ");
-    scanf("%c",&dis);
     do{
-        scanf("%c",&temp);
-    }while(temp!='\n');
+        scanf("%c",&dis);
+    }while(dis=='\n');
     if(dis=='Y'||dis=='y'){
          printf("Enter data : ");
          scanf("%d",&newNode->data);
+         printf("You are successfully enter %d \n",newNode->data);
          newNode->left=create();
          newNode->right=create();
+         return newNode;
     }
     else
         return NULL;
 }
+void preOrder(struct node *root){
+    if(root==NULL)
+        return;
+    printf("%d ",root->data);
+    preOrder(root->left);
+    preOrder(root->right);
+}
+void postOrder(struct node *root){
+    if(root==NULL)
+        return;
+    preOrder(root->left);
+    preOrder(root->right);
+    printf("%d ",root->data);
+}
+void inOrder(struct node *root){
+    if(root==NULL)
+        return;
+    preOrder(root->left);
+    printf("%d ",root->data);
+    preOrder(root->right);  
+}
 void main(){
-    struct node *head=NULL;
-    head=create();
+    struct node *root=NULL;
+    root=create();
+    printf("\nINORDER\n");
+    inOrder(root);
+    printf("\nPREORDER\n");
+    preOrder(root);
+    printf("\nPOSTORDER\n");
+    postOrder(root);
 }
